4acca1f257620efd16bfb945b5aedc8fd3b787be
Jigar Jain <jjain91@gmail.com>
2014-03-10 01:41:05 +0530

    Simple Design of Resume in Foundation 5.2

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="noindex, nofollow" />
+        <title>Jigar Jain | Profile</title>
+        <link rel="stylesheet" href="assets/css/foundation.min.css" />
+        <link rel="stylesheet" href="assets/css/app.css" />
+    </head>
+
+   <body>
+        <div class="row">
+            <div class="small-12 columns">
+                <div class="title">
+                    <h1><a href="http://jigarjain.com/">Jigar Jain</a></h1>
+                    <h6><em>Web Artisan</em></h6>
+                </div>
+                <p>
+                    I am a full stack programmer from Mumbai(India) having about 2 years experience in layers of web technologies including front-end, developing business logic, 
+                    forming DB models & deploying the application on cloud. I try to build simple programming logic & even simpler designs.
+                </p>
+
+                <div class="timeline">
+                    <h4 class="heading">Work Timeline</h4>
+                    <ul>
+                        <li>
+                            <a href="">Kodeplay</a> - Pursued my internship during my final year where i was introduced to OpenCart. 
+                            Built a basic Mobile template of default Opencart site. Also developed our BE project under their guidance.
+                            <span class="time">2011 (6 months)</span>
+                        </li>
+                        <li>
+                            <a href="#">Master Software Solutions</a> - Designing emailers & landing pages for clients like ICICI & Godrej.
+                            <span class="time">2012 (3 months)</span>
+                        </li>
+                        <li>
+                            <a href="#">ProjectHeena</a> - Overlooked the Corporate job offer & joined ProjectHeena as first employee to make impact
+                            in society by trying to make volunteering for non-profits more cool. I am the part of complete tech cycle along with some volunteering.
+                            <span class="time">2012 - Current</span>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="skills">
+                    <h4 class="heading">Skills & Frameworks</h4>
+                    <ul>
+                        <li><span class="success label">PHP</span></li>
+                        <li><span class="success label">Codeigniter</span></li>
+                        <li><span class="success label">MySQL</span></li>
+                        <li><span class="success label">HTML/CSS3</span></li>
+                        <li><span class="success label">jQuery</span></li>
+                        <li><span class="success label">Zurb Foundation</span></li>
+                        <li><span class="success label">Bootstrap</span></li>
+                        <li><span class="label">Apache</span></li>
+                        <li><span class="label">Nginx</span></li>
+                        <li><span class="label">Wordpress</span></li>
+                        <li><span class="label">Opencart</span></li>
+                        <li><span class="secondary label">Laravel</span></li>
+                        <li><span class="secondary label">NodeJs</span></li>
+                    </ul>
+                </div>
+
+                <div class="activities">
+                    <h4 class="heading">Others</h4>
+                    <ul>
+                        <li>Blogging including a <a href="#">guest post</a> on DigitalOcean about installing Opencart on apache.</li>
+                        <li>Completing a <a href="#">Social Psychology course</a> on Coursera.</li>
+                        <li>Volunteering for <a href="#">several NGOs</a> & earning some good karma.</li>
+                    </ul>
+                </div>
+
+                <div class="wishlist">
+                    <h4 class="heading">Wishlist</h4>
+                    <ul>
+                        <li>Learning process automation & scaling in Web technologies.</li>
+                        <li>Watch more movies than last year.</li>
+                        <li>Read atleast 6 <em>non-programming</em> books.</li>
+                    </ul>
+                </div>
+
+                <div class="row">
+                    <div class="small-8 small-centered columns">
+                        <hr>
+                    </div>
+                </div>
+
+                <div class="social">
+                    <a href="https://facebook.com/jjain91" class="fb"><i class="fa fa-facebook"></i></a>
+                    <a href="https://twitter.com/ragij" class="tw"><i class="fa fa-twitter"></i></a>
+                    <a href="https://plus.google.com/+jigarjain1" class="gp"><i class="fa fa-google-plus"></i></a>
+                    <a href="https://bitbucket.org/jigarjain" class="bb"><i class="fa fa-bitbucket"></i></a>
+                    <a href="stackoverflow.com/users/1019746/jigar" class="so"><i class="fa fa-stack-overflow"></i></a>
+                    <br>
+                </div>
+                <p class="last-update">Last updated on 15th Feb 2014</p>
+            </div>
+        </div>
+
+        <link rel="stylesheet" href="assets/thirdparty/fa/css/font-awesome.min.css" />
+    </body>
+</html>

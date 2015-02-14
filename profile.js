6becd7cf279ea94f34e05bb642e5613270a6fa4b
Jigar Jain <jigar@grallo.com>
2015-02-14 20:12:56 +0530

    Blog entry updated

--- profile.js
+++ profile.js
@@ -6,14 +6,19 @@ module.exports = {
             'title': 'Articles',
             'content': [
                 {
+                    'text': 'My Vow of Silence',
+                    'url': 'http://jigarjain.com/blog/vow-of-silence/',
+                    'date': '14th Feb, 15'
+                },
+                {
                     'text': '2014: Year in Review',
                     'url': 'http://jigarjain.com/blog/2014-year-review/',
-                    'date': '31st Dec, 14 '
+                    'date': '31st Dec, 14'
                 },
                 {
                     'text': 'Sums up my last 2 hours',
                     'url': 'http://jigarjain.com/blog/sums-last-2-hours/',
-                    'date': '6th Mar, 14 '
+                    'date': '6th Mar, 14'
                 },
                 {
                     'text': 'Hiding email address in ...',
@@ -24,11 +29,6 @@ module.exports = {
                     'text': 'Moral Reasoning & the Trolley Problem',
                     'url': 'http://jigarjain.com/blog/moral-reasoning-trolley-problem/',
                     'date': '24th Jan, 14'
-                },
-                {
-                    'text': 'DYK: Simple way to get data uri ...',
-                    'url': 'http://jigarjain.com/blog/simple-way-to-get-data-uri-of-image/',
-                    'date': '23rd Jan, 14'
                 }
             ],
             'metaLink': {
